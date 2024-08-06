9c2aad49af63308692e33b7580f85ec9b6dd2511
PedroP7l <pedro_paulo.7l@.com>
2024-08-06 20:40:38 -0300

    implementando endpoints de tarefas

--- fast_api/app.py
+++ fast_api/app.py
@@ -1,14 +1,17 @@
-# https://youtu.be/JzJYWQ6wBAE?list=PLOQgLBuj2-3IuFbt-wJw2p2NiV9WTRzIP&t=4661
+# https://youtu.be/yrTVzukvOJM?list=PLOQgLBuj2-3IuFbt-wJw2p2NiV9WTRzIP&t=6619
+# olhar o parametrize para os testes
+# corrigir a rota get do TODOS
 from http import HTTPStatus
 
 from fastapi import FastAPI
 
-from fast_api.routers import auth, users
+from fast_api.routers import auth, todo, users
 from fast_api.schemas import Message
 
 app = FastAPI()
 app.include_router(users.router)
 app.include_router(auth.router)
+app.include_router(todo.router)
 
 
 @app.get('/', status_code=HTTPStatus.OK, response_model=Message)
