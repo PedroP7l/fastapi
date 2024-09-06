8df9f70ed2760e9be91e7e6d74556771b7b05b4f
PedroP7l <pedro_paulo.7l@.com>
2024-09-06 14:06:24 -0300

    Corrigindo bug educativo

--- tests/test_app.py
+++ tests/test_app.py
@@ -4,5 +4,5 @@ from http import HTTPStatus
 def test_root_deve_retornar_ok_e_ola_mundo(client):
     response = client.get('/')
 
-    assert response.status_code == HTTPStatus.NOK
+    assert response.status_code == HTTPStatus.OK
     assert response.json() == {'message': 'Olá Mundo!'}
